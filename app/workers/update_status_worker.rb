503d0dbcc65227cb2e63cfe9546749eef196d519
Frank Wu <workhard_smile@163.com>
2016-09-06 17:57:26 +0800

    # insert into testlink.cfield_design_values(field_id,node_id,`value`) # select 1,n.id,'Not Ready for Automation' from testlink.nodes_hierarchy n # where n.node_type_id = 4 and n.parent_id in (select test_link_id from testlink.old_test_cases);

--- app/workers/update_status_worker.rb
+++ app/workers/update_status_worker.rb
@@ -4,9 +4,13 @@ class UpdateStatusWorker
   def perform(test_link_ids)
     puts "(#{test_link_ids.join(',')})"
     if test_link_ids.count > 0
+      # delete FROM testlink.cfield_design_values;
+      # insert into testlink.cfield_design_values(field_id,node_id,`value`) 
+      # select 1,n.id,'Not Ready for Automation' from testlink.nodes_hierarchy n
+      # where n.node_type_id = 4 and n.parent_id in (select test_link_id from testlink.old_test_cases);
       query_string ="update cfield_design_values v
         set v.value = 'Automated'
-        where v.field_id = 8
+        where v.field_id = 1
         and v.node_id in (
         select n.id from nodes_hierarchy n
         where n.parent_id in (#{test_link_ids.join(',')})
